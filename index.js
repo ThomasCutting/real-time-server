2c71075a60bbcf76706dbeb668b8839aec1ca160
Thomas Cutting <tom21212@gmail.com>
2016-07-16 19:13:21 -0700

    Added the requirement for the DOTENV.  Created the high-level abstraction layer that controls our realtime server.

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 	Thrame Realtime Server.
 	V-Alpha?
 */
+
+// for our environment, etc.
+require('dotenv').config();
+
+// bring in our CORE application.
 var application = require('./app/core');
 
 // This is implemented within our application container.
 application.registerDependencies();
-
-// This is also implemented within the application container.
-application.startServer();
-
-// This is also implemented within the application container.
+application.instansiateRedis();
+application.startServer(process.env.SERVER_PORT);
 application.registerSocketIoListener();
 application.subscribeToCorrectRedisChannel();
 application.registerRedisListener();
 
-application.log("Started Server"); 
+application.info("Application Registry & Enqueue Process, Completed."); 
