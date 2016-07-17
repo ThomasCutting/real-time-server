16bebd17c955e438a225a825d9c029452e200f70
Thomas Cutting <tom21212@gmail.com>
2016-07-16 20:42:56 -0700

    Added a few small comments inside the index.js file.

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ var application = require('./app/core');
 
 // This is implemented within our application container.
 application.registerDependencies();
+
+// Instansiate redis and start server.
 application.instansiateRedis();
 application.startServer(process.env.SERVER_PORT);
+
+// Register socket.io and subscribe, and register to redis listener.
 application.registerSocketIoListener();
 application.subscribeToCorrectRedisChannel();
 application.registerRedisListener();
 
+// Make sure that our application knows our registry and enqueue process is complete.
 application.info("Application Registry & Enqueue Process, Completed."); 
